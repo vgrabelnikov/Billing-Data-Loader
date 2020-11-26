5dcf088b3f1b51d0a2d0c074b2052341f2e602ea
Vsevolod Grabelnikov <vsgrab@yandex-team.ru>
2020-11-26 17:36:53 +0300

    fixed problem with resource_id position

--- main.py
+++ main.py
@@ -9,42 +9,43 @@ import os
 
 CH_PASSWORD = os.environ['CH_PASSWORD']
 CH_HOST = 'https://{host}:8443/?database={db}'.format(
-        host= os.environ['CH_HOST'],
-        db= os.environ['CH_DB'])
+    host=os.environ['CH_HOST'],
+    db=os.environ['CH_DB'])
 AUTH = {
-        'X-ClickHouse-User': os.environ['CH_USER'],
-        'X-ClickHouse-Key': CH_PASSWORD,
-       }
+    'X-ClickHouse-User': os.environ['CH_USER'],
+    'X-ClickHouse-Key': CH_PASSWORD,
+}
 CERT = './CA.pem'
 BUCKET = os.environ['STORAGE_BUCKET']
 FOLDER = os.environ['STORAGE_FOLDER']
 TABLE = os.environ['CH_TABLE']
 columns = [
-            'billing_account_id',
-            'billing_account_name',
-            'cloud_id',
-            'cloud_name',
-            'folder_id',
-            'folder_name',
-            'resource_id',
-            'service_id',
-            'service_name',
-            'sku_id',
-            'sku_name',
-            'date',
-            'currency',
-            'pricing_quantity',
-            'pricing_unit',
-            'cost',
-            'credit',
-            'monetary_grant_credit',
-            'volume_incentive_credit',
-            'cud_credit',
-            'misc_credit',
-            'locale',
-            'updated_at',
-            'exported_at'
+    'billing_account_id',
+    'billing_account_name',
+    'cloud_id',
+    'cloud_name',
+    'folder_id',
+    'folder_name',
+    'resource_id',
+    'service_id',
+    'service_name',
+    'sku_id',
+    'sku_name',
+    'date',
+    'currency',
+    'pricing_quantity',
+    'pricing_unit',
+    'cost',
+    'credit',
+    'monetary_grant_credit',
+    'volume_incentive_credit',
+    'cud_credit',
+    'misc_credit',
+    'locale',
+    'updated_at',
+    'exported_at'
 ]
+
 
 def request():
     url = (CH_HOST + '&query={query}').format(
@@ -84,18 +85,18 @@ def upload(table, content, host=CH_HOST):
     else:
         raise ValueError(r.text)
 
+
 # Shape Data
 def shape_df(tmp_df):
-
     shaped_df = pd.DataFrame(columns=columns)
     for col in columns:
         try:
-            shaped_df[col]=tmp_df[col]
+            shaped_df[col] = tmp_df[col]
         except KeyError as cerr:
             shaped_df[col] = ""
 
     shaped_df["date"] = pd.to_datetime(shaped_df["date"]).dt.round('D')
-    #tmp_df["exported_at"] = tmp_df["exported_at"].dt.round('s')
+    # tmp_df["exported_at"] = tmp_df["exported_at"].dt.round('s')
 
     decimal_columns: List[str] = [
         'pricing_quantity',
@@ -108,15 +109,15 @@ def shape_df(tmp_df):
     ]
 
     for col in decimal_columns:
-            shaped_df[col] = pd.to_numeric(shaped_df[col])
-            shaped_df[col] = shaped_df[col].round(10)
+        shaped_df[col] = pd.to_numeric(shaped_df[col])
+        shaped_df[col] = shaped_df[col].round(10)
 
     return shaped_df
 
-def init(drop=False):
 
+def init(drop=False):
     if (drop):
-        q='''drop table if exists '''  + TABLE + ''' on cluster '{cluster}';'''
+        q = '''drop table if exists ''' + TABLE + ''' on cluster '{cluster}';'''
         get_clickhouse_data(q)
 
     q = '''
@@ -126,9 +127,9 @@ def init(drop=False):
             billing_account_name   String,
             cloud_id String,
             cloud_name String, 
-            resource_id String,
             folder_id String,
             folder_name    String,
+            resource_id String,
             service_id String,
             service_name String,   
             sku_id String,
@@ -153,13 +154,13 @@ def init(drop=False):
         '''
     get_clickhouse_data(q)
 
+
 def clear_part(part_dt):
     q = '''ALTER TABLE ''' + TABLE + ''' DROP PARTITION ''' + pd.to_datetime(part_dt).strftime("'%Y-%m-%d'")
     get_clickhouse_data(q)
 
 
 def reload(event, context):
-
     init(True)
 
     # q = '''select concat(replace(toString(
@@ -176,9 +177,9 @@ def reload(event, context):
         endpoint_url='https://storage.yandexcloud.net'
     )
 
-    kwargs = {"Bucket": BUCKET, "Prefix" : FOLDER, "MaxKeys" : 100, "StartAfter" : FOLDER + '/' + start_key}
+    kwargs = {"Bucket": BUCKET, "Prefix": FOLDER, "MaxKeys": 100, "StartAfter": FOLDER + '/' + start_key}
     continuation_token = None
-    bck_cnt=0
+    bck_cnt = 0
     while True:
         if continuation_token:
             kwargs['ContinuationToken'] = continuation_token
@@ -186,20 +187,21 @@ def reload(event, context):
         for key in obj_list['Contents']:
             try:
                 get_object_response = s3.get_object(Bucket=BUCKET, Key=key['Key'])
-                df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')), usecols= lambda x: x in columns)
+                df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')),
+                                 usecols=lambda x: x in columns)
                 df = shape_df(df)
                 for part_dt in df["date"].unique():
-                   clear_part(part_dt)
+                    clear_part(part_dt)
                 upload(
                     TABLE,
                     df.to_csv(index=False, sep='\t'))
-                print('object '+ key['Key'] + ' uploaded' )
+                print('object ' + key['Key'] + ' uploaded')
                 bck_cnt = bck_cnt + 1
             except KeyError as err:
-                print ('object '+ key['Key'] + ' error: ' + str(err))
-                #print ('No objects found in Bucket ' + BUCKET + ' with prefix ' + FOLDER)
+                print('object ' + key['Key'] + ' error: ' + str(err))
+                # print ('No objects found in Bucket ' + BUCKET + ' with prefix ' + FOLDER)
             except ValueError as err:
-                print ('object '+ key['Key'] + ' error: ' + str(err))
+                print('object ' + key['Key'] + ' error: ' + str(err))
         if not obj_list.get('IsTruncated'):  # At the end of the list?
             break
         continuation_token = obj_list.get('NextContinuationToken')
@@ -209,8 +211,8 @@ def reload(event, context):
         'isBase64Encoded': False,
     }
 
-def increment(event, context):
 
+def increment(event, context):
     init(False)
     session = boto3.session.Session()
     s3 = session.client(
@@ -223,7 +225,7 @@ def increment(event, context):
         cur_object = record['details']['object_id']
 
         get_object_response = s3.get_object(Bucket=cur_bucket, Key=cur_object)
-        df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')), usecols= lambda x: x in columns)
+        df = pd.read_csv(StringIO(get_object_response['Body'].read().decode('utf-8')), usecols=lambda x: x in columns)
         df = shape_df(df)
         for part_dt in df["date"].unique():
             clear_part(part_dt)
@@ -234,11 +236,12 @@ def increment(event, context):
 
     return {
         'statusCode': 200,
-      #  'body': str(bck_cnt) + ' objects loaded',
+        #  'body': str(bck_cnt) + ' objects loaded',
         'isBase64Encoded': False,
     }
-def handler(event, context):
 
+
+def handler(event, context):
     try:
         method = event['queryStringParameters']['method']
     except KeyError as err:
@@ -251,7 +254,7 @@ def handler(event, context):
         cur_bucket = event['messages'][0]['details']['bucket_id']
         cur_object = event['messages'][0]['details']['object_id']
     except KeyError as err:
-        print ("Wrong function call")
+        print("Wrong function call")
         return {
             'statusCode': 400,
             'body': 'Wrong function call - either pass method=reload as a parameter or use s3 trigger',
@@ -267,28 +270,26 @@ def handler(event, context):
             'isBase64Encoded': False,
         }
 
-
-#reload('','')
-# handler(json.loads("""{
-#   "messages": [
-#     {
-#       "event_metadata": {
-#         "event_id": "bb1dd06d-a82c-49b4-af98-d8e0c5a1d8f0",
-#         "event_type": "yandex.cloud.events.storage.ObjectDelete",
-#         "created_at": "2019-12-19T14:17:47.847365Z",
-#         "tracing_context": {
-#           "trace_id": "dd52ace79c62892f",
-#           "span_id": "",
-#           "parent_span_id": ""
-#         },
-#         "cloud_id": "b1gvlrnlei4l5idm9cbj",
-#         "folder_id": "b1g88tflru0ek1omtsu0"
-#       },
-#       "details": {
-#         "bucket_id": "archbilling",
-#         "object_id": "yc-billing-export-with-resources/20200616.csv"
-#       }
-#     }
-#   ]
-# }
-# """), '')
+handler(json.loads("""{
+  "messages": [
+    {
+      "event_metadata": {
+        "event_id": "bb1dd06d-a82c-49b4-af98-d8e0c5a1d8f0",
+        "event_type": "yandex.cloud.events.storage.ObjectDelete",
+        "created_at": "2019-12-19T14:17:47.847365Z",
+        "tracing_context": {
+          "trace_id": "dd52ace79c62892f",
+          "span_id": "",
+          "parent_span_id": ""
+        },
+        "cloud_id": "b1gvlrnlei4l5idm9cbj",
+        "folder_id": "b1g88tflru0ek1omtsu0"
+      },
+      "details": {
+"bucket_id": "vsgrab-dev",
+        "object_id": "test.csv"
+      }
+    }
+  ]
+}
+"""), '')
