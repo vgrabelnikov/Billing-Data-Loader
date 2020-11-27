422d0054f0bab52a57007d417d623012e830df21
Vsevolod Grabelnikov <vsgrab@yandex-team.ru>
2020-11-27 20:16:04 +0300

    added test.py for easy testing from local machine

--- main.py
+++ main.py
@@ -270,27 +270,3 @@ def handler(event, context):
             'body': 'Wrong function call - either pass method=reload as a parameter or use s3 trigger',
             'isBase64Encoded': False,
         }
-
-# handler(json.loads("""{
-#   "messages": [
-#     {
-#       "event_metadata": {
-#         "event_id": "bb1dd06d-a82c-49b4-af98-d8e0c5a1d8f0",
-#         "event_type": "yandex.cloud.events.storage.ObjectDelete",
-#         "created_at": "2019-12-19T14:17:47.847365Z",
-#         "tracing_context": {
-#           "trace_id": "dd52ace79c62892f",
-#           "span_id": "",
-#           "parent_span_id": ""
-#         },
-#         "cloud_id": "b1gvlrnlei4l5idm9cbj",
-#         "folder_id": "b1g88tflru0ek1omtsu0"
-#       },
-#       "details": {
-# "bucket_id": "vsgrab-dev",
-#         "object_id": "test.csv"
-#       }
-#     }
-#   ]
-# }
-# """), '')
